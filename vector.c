3b547141c1ec264d30866dfed96b726c6f3eda38
Adalberto Caldeira Brant Filho <adalbertobrant@gmail.com>
2018-10-19 21:26:10 -0300

    Create vector.c
    Simple program which creates a vector.

---
+++ vector.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+int main(void){
+
+  int vector [5] ={1,2,3,4,5};
+  int i;
+  for(i=0;i<5;i++){
+  printf("Position in the vector[%d] = %d\n",i,vector[i]);
+  }
+  return 0;
+  }
